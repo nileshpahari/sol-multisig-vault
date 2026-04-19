d54f5b89c4b7f4342c277126ea226e2759de8388
Nilesh Kumar Pahari <nileshpahari@protonmail.com>
2026-04-19 06:31:21 +0530

    add app logic

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import router from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/vault", router);
+
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => {
+  console.log("Started listening on port: ", PORT);
+});
